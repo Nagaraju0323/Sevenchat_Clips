e8dcf39f72026dcc076e48572b28e45f78189bc9
nagarajusevenchats <nagaraju@sevenchats.com>
2022-02-07 09:48:57 +0530

    updateCode

--- Sevenchats/ServiceAndConfiguration/APIRequest.swift
+++ Sevenchats/ServiceAndConfiguration/APIRequest.swift
@@ -2060,7 +2060,7 @@ extension APIRequest {
         
         let dict : [String:Any]  =  [
             "user_id":user_id?.description as Any,
-            "page" : 1,
+            "page" : page,
             "limit" : CLimitTT
             
         ]
@@ -2095,7 +2095,7 @@ extension APIRequest {
         let dict : [String:Any]  =  [
             "friend_id":appDelegate.loginUser?.user_id.description ?? "",
             "user_id":user_id?.description as Any,
-            "page" : 1,
+            "page" : page,
             "limit" : CLimitTT
            
         ]
